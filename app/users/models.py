e66e8207d669f8db988e603042b4b970a15cb5d6
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-10-24 22:13:41 +0300

    email verification

--- app/users/models.py
+++ app/users/models.py
@@ -2,7 +2,7 @@ from cassandra.cqlengine.models import Model
 import uuid
 from cassandra.cqlengine import columns
 from app.config import get_settings
-
+from . import validators 
 settings = get_settings()
 keyspace = settings.keyspace
 
@@ -19,4 +19,21 @@ class User(Model):
     
     def __str__(self):
         return self.__repr__()
+    
+
+    @staticmethod
+    def create_user(email,password=None):
+        # check if their is an email in the database arelady 
+        q = User.objects.filter(email=email) 
+        if q.count() != 0:
+            raise Exception("user already has account")
+        valid , msg , email = validators._validate_email(email)
+        if not valid:
+            raise Exception(msg)
+        obj = User(email= email)
+        obj.password = password
+        obj.save() 
+        return obj 
+    
+
     
