e74f08d23b8a1da6e928f651f7cb396cde683b58
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-10-26 21:59:36 +0300

    handling erros

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ import pathlib
 from fastapi.templating import Jinja2Templates
 from cassandra.cqlengine.management import sync_table 
 from app.users.models import User
+from .users.schemas import UserSignupSchema
+import json
+from pydantic.v1.error_wrappers import ValidationError
+
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 TEMPLATE_DIR = BASE_DIR / "templates"
 
@@ -62,8 +66,23 @@ def login_get_view(request:Request):
 @app.post("/signup",response_class=HTMLResponse) 
 def login_get_view(request:Request,email:str = Form(...),password : str = Form(...),password_confirm: str = Form(...)):
 
+    erros = []
+    error_str= ""
+    try:
+        cleaned_data = UserSignupSchema(email=email,password = password, password_confirm = password_confirm)
+        data = cleaned_data.dict() # if no erros occurs 
+    except ValidationError as e:
+        data = {}
+        error_str =e.json() # if the error occurs 
+        try:
+            erros = json.loads(error_str)
+        except Exception as e:
+            erros = {"loc": "non_field_error","msg":"unknown error"}
+
     return templates.TemplateResponse("auth/signup.html",{
-        "request":request
+        "request":request,
+        "data":data,
+        "errors":erros
     }
     )
 
