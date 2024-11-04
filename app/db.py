7d83b6ea2ccd7ef7ed078ed059d7242e6f5de4a1
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-11-04 22:35:18 +0300

    madesome funny staf here

--- app/db.py
+++ app/db.py
@@ -1,25 +1,19 @@
+
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
-import json
-import pathlib # help me locate location of files 
-from . import config 
-from cassandra.cqlengine import connection
+import os
 
-settings = config.get_settings()
-ASTRADB_CONNECT_BUNDLE_PATH = settings.base_dir.parent   
-
-ASTRADB_CONNECT_BUNDLE =ASTRADB_CONNECT_BUNDLE_PATH  / "connect_bundle"/"connect.zip"
-
-ASTRADB_CLIENT_ID = settings.ASTRADB_CLIENT_ID # AUTHENTICATION TO CASSANDR
-ASTRADB_CLIENT_SECRET = settings.ASTRADB_CLIENT_SECRET # AUTHENTICATION TO CASSANDRA
 def get_session():
-    cloud_config= {
-    'secure_connect_bundle': ASTRADB_CONNECT_BUNDLE
+    cloud_config = {
+        'cloud_config': {  # Assuming AstraDB configuration is provided through env vars
+            'username': os.environ.get('CASSANDRA_USERNAME'),
+            'password': os.environ.get('CASSANDRA_PASSWORD'),
+            'hostname': os.environ.get('CASSANDRA_HOSTNAME'),
+            'port': os.environ.get('CASSANDRA_PORT'),
+        }
     }
 
-    auth_provider = PlainTextAuthProvider(ASTRADB_CLIENT_ID, ASTRADB_CLIENT_SECRET)
+    auth_provider = PlainTextAuthProvider(cloud_config['cloud_config']['username'], cloud_config['cloud_config']['password'])
     cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
     session = cluster.connect()
-    connection.register_connection(str(session),session=session)
-    connection.set_default_connection(str(session))
     return session
