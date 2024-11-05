dde870cbde01bc707c14224e07790abe80e0c9ac
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2024-11-05 17:39:47 +0300

    added some minor styles

--- app/utils.py
+++ app/utils.py
@@ -11,6 +11,7 @@ def valid_schema_data_or_error(raw_data: dict, SchemaModel:BaseModel):
         data = cleaned_data.dict()
     except error_wrappers.ValidationError as e:
         error_str = e.json()
+    
     if error_str is not None:
         try:
             errors = json.loads(error_str)
